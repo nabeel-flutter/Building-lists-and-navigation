b5775d48f58eb0f3d1b21f33ccfe29ab2893bb56
Muhammad Nabeel <nabeelshakeel966@gmail.com>
2024-07-30 15:03:28 +0500

    Initial Commit

---
+++ Building lists and navigation/Building_lists_and_navigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Building_lists_and_navigationApp.swift
+//  Building lists and navigation
+//
+//  Created by apple on 30/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct Building_lists_and_navigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
