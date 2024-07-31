4c7a0bd4332c92ad2e4197703930b931519d2eb5
Muhammad Nabeel <nabeelshakeel966@gmail.com>
2024-07-31 13:10:37 +0500

    completed navigation

--- Building lists and navigation/ContentView.swift
+++ Building lists and navigation/ContentView.swift
@@ -1,6 +1,6 @@
 //
 //  ContentView.swift
-//  Building lists and navigation
+//  Hello World
 //
 //  Created by apple on 30/07/2024.
 //
@@ -10,12 +10,41 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            MapView()
+                       .frame(height: 300)
+
+
+            CircleImage()
+                      .offset(y: -130)
+                      .padding(.bottom, -130)
+
+            VStack(alignment: .leading) {
+                
+                Text("Turtle Rock")
+                    .font(.title)
+                HStack {
+                    Text("Joshua Tree National Park")
+                        .multilineTextAlignment(.leading)
+                    .lineLimit(nil)
+                    Spacer()
+                    Text("California")
+                        .multilineTextAlignment(.leading)
+                    .lineLimit(nil)
+                    
+                }    .font(.subheadline)
+                    .foregroundStyle(.secondary)
+
+                Divider()
+
+
+                Text("About Turtle Rock")
+                    .font(.title2)
+                Text("Descriptive text goes here.")
+
+            }
+            .padding()
+            Spacer()
         }
-        .padding()
     }
 }
 
